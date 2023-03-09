0153e2a953576e50b0e68e72b0ff2e758b6b74a2
mgregerson <mgregerson4321@gmail.com>
2023-03-09 15:15:33 -0500

    Completed Root page and request to questions page

--- app.py
+++ app.py
@@ -7,3 +7,23 @@ app.config['SECRET_KEY'] = "never-tell!"
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
 debug = DebugToolbarExtension(app)
+
+responses = []
+
+@app.get('/')
+def survey_start():
+    """Show start of Survey"""
+
+    return render_template('survey_start.html', title = survey.title, instructions = survey.instructions)
+
+
+@app.get('/questions/<int:question_number>')
+def ask_question(question_number):
+    """Asks the user a question on a form"""
+
+    print('question-number is', survey.questions[question_number])
+    # question = survey.questions[question-number]
+
+    return render_template('question.html', question = survey.questions[question_number])
+
+
