352fad89fb900c128392bcf0fed925f39593e004
mgregerson <mgregerson4321@gmail.com>
2023-03-09 15:27:58 -0500

    Completed redirect page to start of survey

--- app.py
+++ app.py
@@ -16,14 +16,19 @@ def survey_start():
 
     return render_template('survey_start.html', title = survey.title, instructions = survey.instructions)
 
+@app.post('/begin')
+def begin_survey():
+    """Redirects the user to the start of the survey"""
+
+    return redirect('/questions/0')
 
 @app.get('/questions/<int:question_number>')
 def ask_question(question_number):
     """Asks the user a question on a form"""
 
+    question = survey.questions[question_number]
     print('question-number is', survey.questions[question_number])
     # question = survey.questions[question-number]
 
-    return render_template('question.html', question = survey.questions[question_number])
-
+    return render_template('question.html', question = question, choices = question.choices)
 
